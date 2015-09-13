a8c88a59842355740a1956dc79175031d2d03d99
Paul Tsochantaris <ptsochantaris@iBru-5K.default>
2015-09-13 15:39:47 +0100

    Nicer syntax tweak

--- Shared/Globals.swift
+++ Shared/Globals.swift
@@ -133,7 +133,7 @@ enum RepoDisplayPolicy: Int {
 	case Hide, Mine, MineAndPaticipated, All
 	static let labels = ["Hide", "Mine", "Participated", "All"]
 	static let prefixLabels = ["Hide", "Only My", "Participated", "All"]
-	static let policies = [RepoDisplayPolicy.Hide, RepoDisplayPolicy.Mine, RepoDisplayPolicy.MineAndPaticipated, RepoDisplayPolicy.All]
+	static let policies = [Hide, Mine, MineAndPaticipated, All]
 	static let colors = [	COLOR_CLASS(red: 0.8, green: 0.8, blue: 0.8, alpha: 1.0),
 							COLOR_CLASS(red: 0.7, green: 0.0, blue: 0.0, alpha: 1.0),
 							COLOR_CLASS(red: 0.8, green: 0.4, blue: 0.0, alpha: 1.0),
