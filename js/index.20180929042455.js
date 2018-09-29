0344a8e92f6a1f3b087ce28d5d4944b15bf39660
Paul Tsochantaris <ptsochantaris@icloud.com>
2018-09-29 04:26:31 +0100

    Version 1.6.12 release

--- js/index.20180929042455.js
+++ js/index.20180929042455.js
@@ -3,16 +3,16 @@ $.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof att
 
 
 $(document).ready(function() {
-r=function(){dpi=window.devicePixelRatio;$('.js9').attr('src', (dpi>1) ? ((dpi>2) ? 'images/iu-240.png' : 'images/iu-160.png') : 'images/iu-80.png');
+r=function(){dpi=window.devicePixelRatio;$('.js9').attr('src', (dpi>1) ? ((dpi>2) ? 'images/iu-240-1.png' : 'images/iu-160-1.png') : 'images/iu-80-1.png');
 $('.js').attr('src', (dpi>1) ? 'images/background-2400.png' : 'images/background-1200.png');
 $('.js2').attr('src', (dpi>1) ? ((dpi>2) ? 'images/imacoutline-180.png' : 'images/imacoutline-120.png') : 'images/imacoutline-60.png');
 $('.js3').attr('src', (dpi>1) ? ((dpi>2) ? 'images/macbookoutline-180.png' : 'images/macbookoutline-120.png') : 'images/macbookoutline-60.png');
 $('.js4').attr('src', (dpi>1) ? ((dpi>2) ? 'images/ipadoutline-177.png' : 'images/ipadoutline-118.png') : 'images/ipadoutline-59.png');
 $('.js5').attr('src', (dpi>1) ? ((dpi>2) ? 'images/iphoneoutline-117.png' : 'images/iphoneoutline-78.png') : 'images/iphoneoutline-39.png');
 $('.js6').attr('src', (dpi>1) ? ((dpi>2) ? 'images/watchoutline-99.png' : 'images/watchoutline-66.png') : 'images/watchoutline-33.png');
-$('.js7').attr('src', (dpi>1) ? ((dpi>2) ? 'images/ipadscreenshot-1230.png' : 'images/ipadscreenshot-820.png') : 'images/ipadscreenshot-410.png');
-$('.js8').attr('src', (dpi>1) ? ((dpi>2) ? 'images/iphonescreenshot-426.png' : 'images/iphonescreenshot-284.png') : 'images/iphonescreenshot-142.png');
-$('.js10').attr('src', (dpi>1) ? ((dpi>2) ? 'images/watchscreenshot-300.png' : 'images/watchscreenshot-200.png') : 'images/watchscreenshot-100.png');};
+$('.js7').attr('src', (dpi>1) ? ((dpi>2) ? 'images/ipadscreenshot-1230-1.png' : 'images/ipadscreenshot-820-1.png') : 'images/ipadscreenshot-410-1.png');
+$('.js8').attr('src', (dpi>1) ? ((dpi>2) ? 'images/iphonescreenshot-426-1.png' : 'images/iphonescreenshot-284-1.png') : 'images/iphonescreenshot-142-1.png');
+$('.js10').attr('src', (dpi>1) ? ((dpi>2) ? 'images/watchscreenshot-300-1.png' : 'images/watchscreenshot-200-1.png') : 'images/watchscreenshot-100-1.png');};
 if(!window.HTMLPictureElement){r();}
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
 
