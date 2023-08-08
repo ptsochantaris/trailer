84e61071469a4b955b28adb3be4eec389913641d
Paul Tsochantaris <ptsochantaris@icloud.com>
2023-08-08 13:24:02 +0100

    Removed dependency on async algorithms

--- Shared/Gate.swift
+++ Shared/Gate.swift
@@ -1,23 +1,37 @@
-import AsyncAlgorithms
+import Lista
 
-struct Gate {
-    private let queue = AsyncChannel<Void>()
+public final actor Gate {
+    private var headroom: Int
+    private let queue = Lista<() -> Void>()
 
     init(tickets: Int) {
-        for _ in 0 ..< tickets {
-            returnTicket()
-        }
+        headroom = tickets
     }
 
     func takeTicket() async {
-        for await _ in queue {
+        guard headroom == 0 else {
+            headroom -= 1
             return
+        }
+        
+        await withCheckedContinuation { continuation in
+            queue.append {
+                continuation.resume()
+            }
         }
     }
 
-    func returnTicket() {
-        Task {
-            await queue.send(())
+    private func _returnTicket() {
+        if let nextInQueue = queue.pop() {
+            nextInQueue()
+        } else {
+            headroom += 1
+        }
+    }
+
+    nonisolated func returnTicket() {
+        Task.detached {
+            await self._returnTicket()
         }
     }
 }
