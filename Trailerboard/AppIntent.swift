57524c6f0e7de09422a45b317aec68494538fdcc
Paul Tsochantaris <ptsochantaris@icloud.com>
2024-10-02 21:30:28 +0100

    Settings maintenance

--- Trailerboard/AppIntent.swift
+++ Trailerboard/AppIntent.swift
@@ -2,8 +2,8 @@ import AppIntents
 import WidgetKit
 
 struct ConfigurationAppIntent: WidgetConfigurationIntent {
-    static var title: LocalizedStringResource = "Configuration"
-    static var description = IntentDescription("This is an example widget.")
+    static let title: LocalizedStringResource = "Configuration"
+    static let description = IntentDescription("This is an example widget.")
 
     // An example configurable parameter.
     @Parameter(title: "Favorite Emoji", default: "😃")
